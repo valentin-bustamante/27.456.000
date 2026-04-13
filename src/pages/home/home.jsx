16337700990752fcacc0750eff12775cc7cd5685
valentin-bustamante <vbedits333@gmail.com>
2026-04-13 16:40:54 -0300

    arreglo

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,13 @@ import { Titulo } from "../../components/titulo/titulo";
 
 const nombreProyecto = "27.456.000 pesos por mes";
 
-<Titulo texto={nombreProyecto} />
+function Home() {
+  return (
+    <div>
+      <Titulo texto={nombreProyecto} />
+      <h2>hola</h2>
+    </div>
+  );
+}
 
 export default Home;
